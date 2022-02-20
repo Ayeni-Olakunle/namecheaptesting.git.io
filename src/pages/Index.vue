c59d5850f6f1eb63d0205271d081c301c3d80b89
Ayeni Olakunle <ayeniolakunle98@gmail.com>
2022-02-20 15:35:13 +0100

    Testing Name Cheap

---
+++ src/pages/Index.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-page class="flex flex-center"> <chartMe />y </q-page>
+</template>
+
+<script>
+import { defineComponent, defineAsyncComponent } from "vue";
+const chartMe = defineAsyncComponent(() =>
+  import("components/charts/mychart.vue")
+);
+export default defineComponent({
+  name: "PageIndex",
+  components: {
+    chartMe,
+  },
+});
+</script>
